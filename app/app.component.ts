bf639528bc6e0e901ce4841fb5a5546c12083b26
mikkeldamm <mikkeldamm@hotmail.com>
2016-05-03 23:52:47 +0200

    more nice style added to simulate stick

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,21 +3,21 @@ import {Component} from '@angular/core';
 import {StickDirective} from './stick.directive';
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <header stick>
-        <h3>This header will stick after default 100px on the Y-axis with class 'stick'</h3>
-    </header>
-    <main>
-        <section>
-            This will become a very large area :)
-        </section>
-        <aside stick [stickMin]="200" [stickClass]="'left-stick'">
-            This could be a box containing filters, menus, itinerary etc.<br>
-            It will get the class 'left-stick' after 200px on the Y-axis
-        </aside>
-    </main>
+    selector: 'my-app',
+    template: `
+        <header stick>
+            <h3>This header will stick after default 100px on the Y-axis with class 'stick'</h3>
+        </header>
+        <main>
+            <section>
+                This will become a very large area :)
+            </section>
+            <aside stick [stickMin]="200" [stickClass]="'left-stick'">
+                This could be a box containing filters, menus, itinerary etc.<br>
+                It will get the class 'left-stick' after 200px on the Y-axis
+            </aside>
+        </main>
     `,
-  directives: [StickDirective]
+    directives: [StickDirective]
 })
 export class AppComponent { }
